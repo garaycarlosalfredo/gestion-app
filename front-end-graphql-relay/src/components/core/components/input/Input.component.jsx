8527ccacf9c8b0fbdf6a0453621fd8236d711168
Carlos <garaycarlosalfredo@gmail.com>
2022-10-23 08:49:15 -0300

    feaqt: add validation input

--- front-end-graphql-relay/src/components/core/components/input/Input.component.jsx
+++ front-end-graphql-relay/src/components/core/components/input/Input.component.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 function Input({ formik, value, type, placeholder, classes }) {
+  const isNotValid =
+    formik.errors[value] && formik.touched[value] && formik.errors[value];
   return (
     <div class={`form-floating mb-3 ${classes}`}>
       <input
-        class={`form-control ${formik.errors[value] ? "is-invalid" : ""}`}
+        class={`form-control ${isNotValid ? "is-invalid" : ""}`}
         id="floatingInput"
         placeholder={placeholder || ""}
         type={type || "text"}
@@ -14,9 +16,7 @@ function Input({ formik, value, type, placeholder, classes }) {
         value={formik.values[value]}
       />
       <label for="floatingInput">{placeholder}</label>
-      <div class="invalid-feedback">
-        {formik.errors[value] && formik.touched[value] && formik.errors[value]}
-      </div>
+      <div class="invalid-feedback">{isNotValid}</div>
     </div>
   );
 }
